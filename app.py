3ef35097fbf9f0f2fbe47bfe9afb3953f0c07642
Hassan Kalantari <hasan.kalantari29@gmail.com>
2025-11-05 17:09:14 +0330

    fix(infinite history): now the model only rememebr the last 10 conversation + the initial system prompt.

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
 import chainlit as cl
 from chatbot_functionality import get_response, update_response_async
 
+MAX_HISTORY = 10
+
 
 @cl.on_chat_start
 def start_chat():
-    cl.user_session.set("message_history", [{"role": "system", "content": "Be angry!"}])
+    cl.user_session.set("message_history", [{"role": "system", "content": "be nice"}])
 
 
 @cl.on_message  # THis will call every time the user input a message in hte UI
 async def main(message: cl.Message):
-
     message_history = cl.user_session.get("message_history")
     message_history.append({"role": "user", "content": message.content})
-    print(message_history)
+
+    # Prevent infinite history
+    message_history = [message_history[0]] + message_history[-MAX_HISTORY:]
 
     result = cl.Message(content="")
     await update_response_async(message_history, message=result)
