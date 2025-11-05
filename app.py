bb3b193dd78f19dbb34c93be2f1baab9d903ae53
Hassan Kalantari <hasan.kalantari29@gmail.com>
2025-11-05 16:36:19 +0330

    feat(chat-bot): 1. Adding stream functionality for better UI/UX. 2. Now the model have a histoy that mimic memory functionality for the model.

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import chainlit as cl
-from chatbot_functionality import get_response
+from chatbot_functionality import get_response, update_response_async
 
 
-@cl.step(type="tool")
-async def tool(message: cl.Message):
-    response = get_response(message)
+@cl.on_chat_start
+def start_chat():
+    cl.user_session.set("message_history", [{"role": "system", "content": "Be angry!"}])
 
-    return response
 
 @cl.on_message  # THis will call every time the user input a message in hte UI
 async def main(message: cl.Message):
-    tool_response = await tool(message)
-    await cl.Message(content=tool_response).send()
+
+    message_history = cl.user_session.get("message_history")
+    message_history.append({"role": "user", "content": message.content})
+    print(message_history)
+
+    result = cl.Message(content="")
+    await update_response_async(message_history, message=result)
+
+    message_history.append({"role": "assistant", "content": result.content})
+    cl.user_session.set("message_history", message_history)
--- chatbot_functionality.py
+++ chatbot_functionality.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from openai import OpenAI
+from openai import OpenAI, AsyncOpenAI
 import chainlit as cl
 
 import os
@@ -15,6 +15,8 @@ client = OpenAI(
     api_key=API_KEY,
 )
 
+async_client = AsyncOpenAI(base_url="https://router.huggingface.co/v1", api_key=API_KEY)
+
 
 def get_response(message: cl.message) -> str:
     completion = client.chat.completions.create(
@@ -25,3 +27,15 @@ def get_response(message: cl.message) -> str:
     response_text = completion.choices[0].message.content
 
     return response_text
+
+
+async def update_response_async(messages, message: cl.Message):
+    stream = await async_client.chat.completions.create(
+        model="openai/gpt-oss-safeguard-20b:groq",
+        stream=True,
+        messages=messages,
+    )
+
+    async for i in stream:
+        if token := i.choices[0].delta.content or "":
+            await message.stream_token(token)
