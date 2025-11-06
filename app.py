5fe47f4c49f3aaa7939cb3ce5753a738471e8383
Hassan Kalantari <hasan.kalantari29@gmail.com>
2025-11-07 00:10:24 +0330

    fix(bug): initial message hostory kept addeing to the existing history. Now it's fixed.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import chainlit as cl
+
 from chatbot_functionality import get_response, update_response_async
 
 MAX_HISTORY = 10
@@ -6,7 +7,7 @@ MAX_HISTORY = 10
 
 @cl.on_chat_start
 def start_chat():
-    cl.user_session.set("message_history", [{"role": "system", "content": "be nice"}])
+    cl.user_session.set("message_history", [{"role": "system", "content": "be so angry!"}])
 
 
 @cl.on_message  # THis will call every time the user input a message in hte UI
@@ -15,7 +16,9 @@ async def main(message: cl.Message):
     message_history.append({"role": "user", "content": message.content})
 
     # Prevent infinite history
-    message_history = [message_history[0]] + message_history[-MAX_HISTORY:]
+    if len(message_history) > MAX_HISTORY + 1:
+        message_history = [message_history[0]] + message_history[-MAX_HISTORY:]
+    print(message_history)
 
     result = cl.Message(content="")
     await update_response_async(message_history, message=result)
